58ec6ce4a00c6a944bab9cf8fb2913806a576377
joy Kurui <kurui.chepngetich@gmail.com>
2023-10-15 20:35:33 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+ buildMyToDo(e.target.querySelector("#text").value)
+ form.reset()
+
+})
 });
+function buildMyToDo(todo){
+  let li = document.createElement('li');
+ let btn = document.createElement('button')
+ btn.addEventListener('click', handleDelete )
+ btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+  console.log(li)
+
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
